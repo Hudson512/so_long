d2f4c564ec9b490b9e1e8a59dbecb726f364a399
Hudson512 <hmateque@gmail.com>
2024-08-20 06:14:19 -0400

    update

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: hmateque <hmateque@student.42luanda.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/07 15:47:53 by hmateque          #+#    #+#             */
-/*   Updated: 2024/08/20 03:23:28 by hmateque         ###   ########.fr       */
+/*   Updated: 2024/08/20 04:30:05 by hmateque         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,11 +33,9 @@ int	main(int ac, char **av)
 				mlx->mlx = mlx_init();
 				mlx->mlx_win = mlx_new_window(mlx->mlx, 900, 800,
 						"Hello world!");
-				mlx->img = mlx_xpm_file_to_image(mlx->mlx, "./img/carro.xpm",
+				mlx->img = mlx_xpm_file_to_image(mlx->mlx, "./img/Taxi_Baixo.xpm",
 						&mlx->img_widt, &mlx->img_heig);
 				mlx_put_image_to_window(mlx->mlx, mlx->mlx_win, mlx->img, 0, 0);
-				mlx_put_image_to_window(mlx->mlx, mlx->mlx_win, mlx->img, 150,
-					60);
 				mlx_loop(mlx->mlx);
 			}
 			else
